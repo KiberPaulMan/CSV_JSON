a190873771ff66a77809309a8e370b6bd6adef26
Paul <putincevpavel777@gmail.com>
2020-11-26 22:59:28 +0700

    Trable with func get_country_code

--- world_population.py
+++ world_population.py
@@ -1,17 +1,30 @@
 import json
-from pygal_maps_world import i18n
+import pygal
+from country_codes import get_country_code
+
 
 # Заполняем список данными
 filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
-    # Вывод населения каждой страны за 2010 год.
+# Построение словаря с данными численности населения
+    cc_population = {}
     for pop_dict in pop_data:
         if pop_dict['Year'] == '2010':
             country_name = pop_dict['Country Name']
             population = int(float(pop_dict['Value']))
-            # print(country_name, pop_dict['Value'])
+            print(population)
+            code = get_country_code(country_name)
+            print(code)
+            if code:
+                cc_population[code] = population
 
-for country_code in sorted(i18n.COUNTRIES.keys()):
-    print(country_code, i18n.COUNTRIES[country_code])
+for code, pop in enumerate(cc_population):
+    print(code, pop)
+
+wm = pygal.maps.world.World()
+wm.title = 'World population in 2010, by Country'
+wm.add('2010', cc_population)
+
+wm.render_to_file('world_population.svg')
